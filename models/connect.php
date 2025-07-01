7080959290a5c6a4eb5792f096fe764c987d2a40
Josthyn4899 <josthyn4899@gmail.com>
2025-06-30 20:46:23 -0600

    Creacion BD
    Cambios en homemodel, connect y cracion de la base de datos con tabla de registrar usuario y su procedimiento almacenado

---
+++ models/connect.php
@@ -0,0 +1,14 @@
+<?php
+//Por hacer base de datos
+    function OpenDB()
+    {
+        return mysqli_connect("127.0.0.1:3307","root","dbproyectoweb");
+    }
+
+    function CloseDB($context)
+    {
+        mysqli_close($context);
+    }
+
+
+?>
--- models/homeModel.php
+++ models/homeModel.php
@@ -2,13 +2,41 @@
   
   function ValidaInicioSesionModel($nombreUsuario,$contrasena){
     //envia los datos a validar a la Base de datos!!
+     try
+        {
+            $context = OpenDB();
 
-    return false;
+            $sp = "CALL ValidarInicioSesion('$correo', '$contrasenna')";
+            $respuesta = $context -> query($sp);
+
+            CloseDB($context);            
+            return $respuesta;
+        }
+        catch(Exception $error)
+        {
+            
+            return null;
+        }
   }
 
   function RegistrarUsuarioModel($nombreUsuario,$numeroTelefono , $correo, $contrasena){
 
     //enviar los datos a registrar a la base de datos!!
+      try
+        {
+            $context = OpenDB();
+
+            $sp = "CALL RegistrarUsuario('$nombre', '$correo', '$identificacion', '$contrasenna')";
+            $respuesta = $context -> query($sp);
+
+            CloseDB($context);            
+            return $respuesta;
+        }
+        catch(Exception $error)
+        {
+            
+            return false;
+        }
   }
 
  ?>
